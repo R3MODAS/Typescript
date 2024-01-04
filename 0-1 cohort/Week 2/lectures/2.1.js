ef36249942b09e6e4d6cbe9642bf94d76dce4ae4
Sharadindu Das <remodas7774@gmail.com>
2024-01-05 00:17:03 +0530

    http server on express done

---
+++ 0-1 cohort/Week 2/lectures/2.1.js
@@ -0,0 +1,12 @@
+//! Basics of Express
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", function(req,res){
+    res.send('Hello World!');
+})
+
+app.listen(port, function(){
+    console.log(`Server is running on http://localhost:${port}`)
+})
