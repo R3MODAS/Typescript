eb7a43e579f42424ffc18a965cbccb404d0d009f
Sharadindu Das <remodas7774@gmail.com>
2024-01-05 16:29:03 +0530

    express basics done

--- 0-1 cohort/Week 2/lectures/2.1.js
+++ 0-1 cohort/Week 2/lectures/2.1.js
@@ -1,10 +1,29 @@
 //! Basics of Express
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+// middlewares
+app.use(bodyParser.json());
+
+// app.get("/", function(req,res){
+//     res.send('Hello World!');
+// })
+
+// we can send html as well
 app.get("/", function(req,res){
-    res.send('Hello World!');
+    res.send('<h1>Hello world</h2>');
+})
+
+app.post("/backend-api/conversations",function (req,res) {
+    // console.log(req.headers.authorization) 
+    // console.log(req.body)
+    const message = req.body.message;
+    console.log(message)
+    res.json({
+        output : "2 + 2 = 4"
+    })
 })
 
 app.listen(port, function(){
