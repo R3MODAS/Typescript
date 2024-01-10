312994aee0a1446ca231cf0cfbe70be56f41d247
Sharadindu Das <remodas7774@gmail.com>
2024-01-10 23:00:00 +0530

    intro to zod

--- 0-1 cohort/Week 3/lectures/index.js
+++ 0-1 cohort/Week 3/lectures/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const zod = require("zod");
 const app = express();
 
 /*
@@ -6,7 +7,7 @@ const app = express();
 2. User should send a username and password in headers
 */
 
-// ugly way of doing it without middleware
+//! ugly way of doing it without middleware
 // app.get("/health-checkup", (req, res) => {
 //   const username = req.headers.username;
 //   const password = req.headers.password;
@@ -47,7 +48,7 @@ const app = express();
 //     })
 // })
 
-// let's create wrapper function to avoid dry principle (repeating code)
+//! let's create wrapper function to avoid dry principle (repeating code)
 
 // function inputValidator(username, password){
 //   if (username != "Remo" || password != "pass") {
@@ -103,52 +104,100 @@ const app = express();
 //     })
 // })
 
-// middlewares
+//! middlewares
 // app.get(route,callback func, callback func, callback func......)
 
-function userMiddleware(req,res,next){
-  const username = req.headers.username;
-  const password = req.headers.password;
+// function userMiddleware(req,res,next){
+//   const username = req.headers.username;
+//   const password = req.headers.password;
 
-  if(username != "Remo" || password != "pass"){
-    res.status(403).json({
-      error:"Invalid Username or Password"
-    })
-  }
-  else{
-    next();
-  }
+//   if(username != "Remo" || password != "pass"){
+//     res.status(403).json({
+//       error:"Invalid Username or Password"
+//     })
+//   }
+//   else{
+//     next();
+//   }
 
-}
+// }
 
-function kidneyMiddleware(req,res,next){
-  const kidneyId = req.query.kidneyId;
+// function kidneyMiddleware(req,res,next){
+//   const kidneyId = req.query.kidneyId;
 
-  if(kidneyId != 1 && kidneyId != 2){
-    res.status(403).json({
-      error : "Invalid input"
-    })
-  }
-  else{
-    next()
-  }
-}
+//   if(kidneyId != 1 && kidneyId != 2){
+//     res.status(403).json({
+//       error : "Invalid input"
+//     })
+//   }
+//   else{
+//     next()
+//   }
+// }
 
-let numberOfRequests = 0;
+// let numberOfRequests = 0;
 
-function calculateRequests(req,res,next){
-  numberOfRequests++;
-  console.log(`Number of requests made so far ${numberOfRequests}`);
-  next()
-}
+// function calculateRequests(req,res,next){
+//   numberOfRequests++;
+//   console.log(`Number of requests made so far ${numberOfRequests}`);
+//   next()
+// }
 
-app.use(calculateRequests)
+// app.use(calculateRequests)
+// app.use(express.json())
+
+// app.get("/health-checkup", userMiddleware, kidneyMiddleware, (req,res) => {
+//   res.json({
+//     message : 'Your heart is healthy'
+//   })
+// })
+
+//! what if we don't use input validation
+// app.use(express.json())
+// app.post("/health-checkup", (req,res) => {
+//   // user may send wrong data and we need handle it
+//   const kidneys = req.body.kidneys;
+//   const numberOfKidneys = kidneys.length;
+//   res.json({
+//     message : `You have ${numberOfKidneys} kidneys`
+//   })
+// })
+
+//! global catches
+// app.use((err,req,res,next) => {
+//   // console.log(err);
+//   res.json({
+//     message : `Some problems has occured on our server`
+//   })
+// })
+
+//! Intro to ZOD
+const schema = zod.array(zod.number());
+
 app.use(express.json())
+app.post("/health-checkup", (req,res) => {
+  const kidneys = req.body.kidneys;
+  const response = schema.safeParse(kidneys);
+  // console.log(response.error)
 
-app.get("/health-checkup", userMiddleware, kidneyMiddleware, (req,res) => {
-  res.json({
-    message : 'Your heart is healthy'
-  })
-})
+  if(!response.success){
+    res.status(411).json({
+      message : `Input is invalid`
+    })
+  }
+  else{
+    res.json({
+      response
+    })
+  }
+}) 
+
+// app.use((err,req,res,next) => {
+//   // console.log(err);
+//   res.json({
+//     message : `Some problems has occured on our server`
+//   })
+// })
+
 
 app.listen(3000, () => console.log(`Server started at http://localhost:3000`))
