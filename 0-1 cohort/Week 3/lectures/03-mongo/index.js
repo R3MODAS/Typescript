b3c830dddcfbf80b65fdcb8ea485d5022796142f
Sharadindu Das <remodas7774@gmail.com>
2024-01-30 19:00:00 +0530

    MongoDB Basic done

--- 0-1 cohort/Week 3/lectures/03-mongo/index.js
+++ 0-1 cohort/Week 3/lectures/03-mongo/index.js
@@ -1,93 +1,112 @@
-const mongoose = require("mongoose")
-const express = require("express");
-const app = express();
+// const express = require("express")
+// const mongoose = require("mongoose")
 
-app.use(express.json())
+// const DB_NAME = "userDB"
+// const DB_URL = "mongodb+srv://remo:remo123@cluster0.zi35bim.mongodb.net"
 
-//! Basic of MongoDB 
-const DB_URL = "mongodb+srv://remo:remo123@cluster0.zi35bim.mongodb.net"
-const DB_NAME = "USER_DB"
-
-// connecting to DB
 // mongoose.connect(`${DB_URL}/${DB_NAME}`)
 
-// creating a schema
+// Schema for the User model/collection 
 // const userSchema = new mongoose.Schema({
-//     username : {
-//         type : String,
-//         required : [true, "username is required"],
-//         unique : true,
-//         lowercase : true
+//     username: {
+//         type: String,
+//         required: [true, "Username is required"],
+//         unique: true,
 //     },
-//     email : {
-//         type : String,
-//         required : [true, "email is required"],
-//         unique : true,
-//         lowercase : true
+//     email: {
+//         type: String,
+//         required: [true, "Email is required"],
+//         unique: true,
+//         lowercase: [true, "Email should be lowercase"]
 //     },
 //     password: {
 //         type: String,
-//         required: [true, 'password is required'],
-//     },
+//         required: [true, "Password is required"],
+//         min: [8, "Password should be more than 8 characters"]
+//     }
 // })
 
-// Schema of mongoose which creates a model/collection
-// const User = mongoose.model("User", userSchema)
+// creating the model/collection named User
+// const User = mongoose.model("User",userSchema)
 
-// Data to insert into DB
-// const user = new User({username : "Remo Das", email : "remo@gmail.com", password : "remo123"})
-// user.save().then(() => console.log(`Data has been saved Successfully`))
+// providing the data to insert into the model
+// const newUser = new User({username: "Remo", email: "remo@gmail.com", password: "remodas7774"})
 
-//! Let's do it with practical example
+// saving the data inside the model of the db
+// newUser.save().then(() => console.log(`Data has been saved successfully !!!`))
+
+//! Practical Approach
+const express = require("express")
+const mongoose = require("mongoose")
+const jwt = require("jsonwebtoken")
+
+const PORT = 3000 || process.env.PORT
+const DB_NAME = "userDB"
+const DB_URL = "mongodb+srv://remo:remo123@cluster0.zi35bim.mongodb.net"
+const jwtPassword = "123456"
+
 mongoose.connect(`${DB_URL}/${DB_NAME}`)
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: [true, "Username is required"],
-        unique: true,
-        lowercase: true
+        unique: true
     },
     email: {
         type: String,
         required: [true, "Email is required"],
         unique: true,
-        lowercase: true
+        lowercase: [true, "Email should be in lowercase"]
     },
     password: {
         type: String,
         required: [true, "Password is required"],
+        unique: true,
+        min: [8, "Password should be more than 8 characters"]
     }
-}, { timestamps: true })
+}, {timestamps: true})
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema)
 
-app.post("/signup", async (req, res) => {
-    const username = req.body.username
-    const email = req.body.email
-    const password = req.body.password
+const app = express()
+app.use(express.json())
 
-    const user = new User({
-        username: username,
-        email: email,
-        password: password
-    })
+app.post("/signup", async(req,res) => {
+    const {username, email, password} = req.body
 
-    // Finding the username exists in the DB or not
-    const existingUser = await User.findOne({ username: username })
+    const existingUser = await User.findOne({username: username})
 
-    if (existingUser) {
-        return res.status(400).json({
-            message: `User already exists`
-        })
-    }
-    else {
-        user.save()
-        res.json({
-            message: `User ${user.username} has been created!`
+    if(existingUser){
+        res.status(403).json({
+            message: 'User already exists in the db !!!'
         })
     }
 
+    else{
+        const createUser = new User({
+            username: username,
+            email: email,
+            password: password
+        })
+
+        createUser.save()
+        .then((data) => {
+            res.status(200).json({
+                message: 'User has been added to the db successfully !!!'
+            })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(400).json({
+                message: 'Something went wrong with the user data !!!'
+            })
+        })
+    }
 })
 
-app.listen(3000, () => console.log(`Server started at http://localhost:3000`))
+app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`))
+
+
+
+
