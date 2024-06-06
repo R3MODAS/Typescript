98253cd2f393b11ffb5cf9fbc432b85ee2a43319
Sharadindu Das <remodas7774@gmail.com>
2024-06-06 14:11:03 +0530

    basics of typescript

---
+++ 01-Basics/index.ts
@@ -0,0 +1,9 @@
+// Basic Primitive Types
+let userName: string = "Remo"
+let userAge: number = 23
+let isValid: boolean = true
+
+// Combining Types
+// Union Types
+let userID: string | number = "randomId"
+userID = 123
